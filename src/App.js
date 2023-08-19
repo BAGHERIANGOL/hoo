3331140c3e4404c7833fc364ae4bb51a19a8ebcf
BAGHERIANGOL <bagheriangl@outlook.com>
2023-08-19 08:16:06 -0700

    part icons
    exact  for icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-  <h1>HOO YAHOO</h1>
+    <div className='App'>
+      <h1>HOO YAHOO</h1>
+
+      <iconify-icon
+        icon='twemoji:astronaut-medium-skin-tone'
+        height='unset'
+        style={{
+          width: '40px',
+          height: '40px',
+        
+          color: 'red',
+        }}
+      />
+  
     </div>
   );
 }
